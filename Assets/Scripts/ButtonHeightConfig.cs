4af43d0d62d677efe5c9c2cea20a958508f5ebd6
AVERYYYYYYYYYYYYYYYY <averyrobin12@outlook.com>
2025-05-27 17:40:28 +0100

    button heigh config start
    just layin' out the foundations of the script, but i can't use the new input system as we're on an older version of unity

---
+++ Assets/Scripts/ButtonHeightConfig.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ButtonHeightConfig : MonoBehaviour
+{
+    [Header("Buttons")]
+    [SerializeField] public GameObject[] Buttons;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (GameObject button in Buttons)
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
