67bc17d61c019d1122af84dac0495cd608b662fa
Matt <122266257+MrGluestickman@users.noreply.github.com>
2025-05-28 15:24:27 +0100

    Button transistion
    setup a button so that when you click it it takes you from the Start screen to the gameplay screen

---
+++ Assets/Scripts/TESTTHINGYMATT.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TESTTHINGYMATT : MonoBehaviour
+{
+    public void DoesButtonWork()
+    {
+        Debug.Log("yo wassup you clicked me fool");
+    }
+}
