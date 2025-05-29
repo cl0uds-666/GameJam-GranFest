67f2bcc7147babaa3b86314dc3141e49717aa579
Josh <cossarjoshua@gmail.com>
2025-05-29 21:54:12 +0100

    Finishlinesounds

---
+++ Assets/Scripts/Finishlinesound.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections;
+
+public class Finishlinesound : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] sfxClips;
+    [SerializeField] private AudioSource audioSource;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        PlayRandomSFX();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        PlayRandomSFX();
+    }
+
+    private void PlayRandomSFX()
+    {
+        if (sfxClips == null || sfxClips.Length == 0 || audioSource == null)
+        {
+            Debug.LogWarning("SFX clips or AudioSource not properly set up!");
+            return;
+        }
+
+        // Get a random index between 0 and the number of clips
+        int randomIndex = Random.Range(0, sfxClips.Length);
+
+        // Play the randomly selected clip
+        audioSource.PlayOneShot(sfxClips[randomIndex]);
+    }
+}
